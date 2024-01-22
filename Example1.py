a799ab7dd34a1e862b3e848be8dc09fc91f3e7cb
Prajwal Niraula <prajwalniraula@dhcp-10-29-213-63.dyn.MIT.EDU>
2024-01-22 11:15:11 -0500

    Download lightcurve functionality added.

---
+++ Example1.py
@@ -0,0 +1,4 @@
+#Created by Prajwal
+from lib.Download import downloadLightcurve
+
+downloadLightcurve(8112039)
---
+++ lib/Download.py
@@ -0,0 +1,25 @@
+
+import pandas as pd
+import lightkurve as lk
+
+import os
+
+def downloadLightcurve(KIC):
+    TargetName = "KIC"+str(KIC) 
+
+    LCSaveDirectory = 'lc_data/'+TargetName
+    if not(os.path.exists(LCSaveDirectory)):
+        os.mkdir('lc_data/'+TargetName)
+
+    Kepler_Files = lk.search_lightcurve(TargetName, mission="Kepler")
+    print(len(Kepler_Files1))
+   
+   
+    #This are for Kepler data    
+    for i in range(len(Kepler_Files)):
+        try:
+            Kepler_Files[i].download(download_dir='lc_data/'+PlanetName)    
+        except:
+            print("No Kepler data found.")
+        
+
